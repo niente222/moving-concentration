eaed1500c269cf311f011e8602075bbbf78c5b07
niente <komanagi12345@gmail.com>
2023-05-06 23:30:26 +0900

    ランキング 日付表示修正

--- src/components/pages/ranking.vue
+++ src/components/pages/ranking.vue
@@ -1,19 +1,19 @@
 <template>
   <h1>ランキング</h1>
   <div
-    v-for="(rank, index) in rankingData"
-    :key="index"
-    class="rankingPanel"
-  >
-    <p>Name: {{ rank.USER_ID }}</p>
-    <p>Turns: {{ rank.TURNS }}</p>
-    <p>Date: {{ rank.INSDATE }}</p>
+      v-for="(rank, index) in rankingData"
+      :key="index"
+      class="ranking-panel"
+    >
+      <p><span class="label">Name:</span> {{ rank.USER_ID }}</p>
+      <p><span class="label">Turns:</span> {{ rank.TURNS }}</p>
+      <p><span class="label">Date:</span> {{ formatDate(rank.INSDATE) }}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import {isLogin} from '@/assets/js/util.js';
+import * as util from '@/assets/js/util.js';
 import * as consts from "@/assets/js/const.js";
 
 export default {
@@ -26,7 +26,7 @@ export default {
   async mounted() {
     try {
         const response = await axios.post("http://localhost:3000/ranking");
-
+        
         if (response.data.success) {
           this.rankingData = response.data.ranking_data.filter(game => game.GAME_LEVEL === consts.GAME_LEVEL_EASY);
           } else {
@@ -42,6 +42,9 @@ export default {
     },
 
     methods: {
+      formatDate(date) {
+      return util.formatDate(date);
+    },
   }
 }
 </script>
