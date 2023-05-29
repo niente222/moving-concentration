8bffb473e9420d84b7d786f7d40dce9e19c243c0
niente <komanagi12345@gmail.com>
2023-05-29 16:54:31 +0900

    不要コード削除

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 import dotenv from 'dotenv';
-import https from 'https';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import express from 'express';
 import cookieSession from'cookie-session';
 import cors from "cors";
@@ -45,16 +41,3 @@ app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
   });
-
-/*var options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/www.ugoku-sinkesuijaku.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/www.ugoku-sinkesuijaku.com/fullchain.pem'),
-}
-
-var server = https.createServer(options, app);
-
-server.listen(443, function() {
-  process.setuid && process.setuid('ec2-user');
-  console.log(`user was replaced to uid: ${process.getuid()} ('ec2-user')`);
-  console.log('example app listening on port 443!');
-});*/
