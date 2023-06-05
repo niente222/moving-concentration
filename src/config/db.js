270972adfdbe74813569e00e47789f4170821737
niente <komanagi12345@gmail.com>
2023-06-05 19:38:49 +0900

    db接続できない修正

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
